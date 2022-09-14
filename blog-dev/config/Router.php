ea5e45e8366e0bca9caa3bb7569108e0c619d087
ladel1 <adel.latibi@gmail.com>
2022-09-14 11:53:52 +0200

    blog dev

--- blog-dev/config/Router.php
+++ blog-dev/config/Router.php
@@ -6,18 +6,12 @@ use Helpers\ReadFile;
     class Router{
 
         public static function route(){
-            if(isset($_GET["page"])){
-                extract($_GET);
-                $routes=ReadFile::readJson("routes.json");
-                $controlerName=$routes[$page][0];
-                $methodName=$routes[$page][1];
-                
-                $instanceController = new $controlerName();
-                $instanceController->$methodName();
-
-            }
-
-
+            $routes=ReadFile::readJson("routes.json");
+            $page = (isset($_GET["page"]))? $_GET["page"] : "";
+            $controlerName=$routes[$page][0];
+            $methodName=$routes[$page][1];                
+            $instanceController = new $controlerName();
+            $instanceController->$methodName();
         }
 
     }
