b18414f71fedb4fd014cc5c703e562c775bee268
ladel1 <adel.latibi@gmail.com>
2022-09-14 16:42:20 +0200

    Session & coockies

--- blog-dev/src/controller/HomeController.php
+++ blog-dev/src/controller/HomeController.php
@@ -1,12 +1,16 @@
 <?php 
 
 namespace App\Controller;
+session_start();
 
 use App\Controller\AbstractController;
 
 class HomeController extends AbstractController{
 
     public function index(){
+        
+        //$_SESSION["user"] = "Adel";
+        var_dump($_SESSION["user"]);
         $title = "xdcdvgbn,;:";
         return $this->renderView("home/index",[
             "title"=>$title,
